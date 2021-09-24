191133ebbcd4d8c6c9fdfb2dca878f0f27e19c1c
Michele Cipriano <micco00x@gmail.com>
2021-09-24 10:29:36 +0200

    Bug fix: theRobotModel not available in constructor on real robot

--- Src/Modules/MotionControl/WalkingEngine/Walk2014Generator.cpp
+++ Src/Modules/MotionControl/WalkingEngine/Walk2014Generator.cpp
@@ -120,14 +120,15 @@ angle_difference(T alpha, T beta) {
 }
 
 Walk2014Generator::Walk2014Generator() {
+  double z_torso = 0.24809; // taken from http://doc.aldebaran.com/2-1/family/robots/links_robot.html
   Pose T_torso_w = Pose(
-      Eigen::Vector3d(0.0, 0.0, 0.24809), // taken from http://doc.aldebaran.com/2-1/family/robots/links_robot.html
+      Eigen::Vector3d(0.0, 0.0, z_torso),
       Eigen::Matrix3d::Identity()
   );
-  Eigen::Vector3d p_lsole_torso = theRobotModel.soleLeft.translation.cast<double>() / mmPerM;
-  Eigen::Matrix3d R_lsole_torso = theRobotModel.soleLeft.rotation.cast<double>();
-  Eigen::Vector3d p_rsole_torso = theRobotModel.soleRight.translation.cast<double>() / mmPerM;
-  Eigen::Matrix3d R_rsole_torso = theRobotModel.soleRight.rotation.cast<double>();
+  Eigen::Vector3d p_lsole_torso = Eigen::Vector3d(0.0, 0.05, -z_torso);
+  Eigen::Matrix3d R_lsole_torso = Eigen::Matrix3d::Identity();
+  Eigen::Vector3d p_rsole_torso = Eigen::Vector3d(0.0, -0.05, -z_torso);
+  Eigen::Matrix3d R_rsole_torso = Eigen::Matrix3d::Identity();
   Pose T_lsole_torso(p_lsole_torso, R_lsole_torso);
   Pose T_rsole_torso(p_rsole_torso, R_rsole_torso);
   Pose T_lsole_w = T_torso_w * T_lsole_torso;
@@ -514,7 +515,7 @@ void Walk2014Generator::calcJoints(WalkGenerator& generator,
   VERIFY(InverseKinematic::calcLegJoints(leftFoot, rightFoot, Vector2f::Zero(), generator.jointRequest, theRobotDimensions) || SystemCall::getMode() == SystemCall::logfileReplay);
 
   // 10. Set joint values and stiffness
-  int stiffness = walkState == standing ? StiffnessData::useDefault : walkStiffness;
+  int stiffness = walking_state_ == WalkingState::Standing ? StiffnessData::useDefault : walkStiffness;
   for(uint8_t i = Joints::firstLegJoint; i < Joints::numOfJoints; ++i)
     generator.jointRequest.stiffnessData.stiffnesses[i] = stiffness;
 
